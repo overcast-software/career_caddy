43b9b00f8dc4aeaaf3347abc3dc97709a1d6af33
file not found <voodoologic@users.noreply.github.com>
2025-08-25 22:07:08 -0700

    example env and mv hardcoded path from code.

--- cli/cli.py
+++ cli/cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
 from openai import OpenAI
 from lib.scrappers.parser_creation import JobSiteParser, get_api_key
@@ -12,6 +13,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="CLI for parsing job site URLs and extracting CSS selectors.")
     parser.add_argument('url', type=str, help='The URL of the job site to parse.')
     parser.add_argument('--api-key', type=str, help='OpenAI API key')
+    parser.add_argument('--resume', type=str, help='Path to the resume file')
     return parser.parse_args()
 
 def main():
@@ -27,7 +29,6 @@ def main():
     # Check if the parser data already exists
     existing_css_selectors = db_handler.fetch_parser_from_url(url)
     if existing_css_selectors:
-        # want to do something here?
         print("CSS Selectors (from database):", existing_css_selectors[0])
     else:
         # Instantiate the OpenAI client
@@ -55,8 +56,13 @@ def main():
     job_description = jp.extract_data_with_selectors(selectors, html)
     db_handler.save_job_description(job_description, job_data_id, url_id)
 
+    # Determine the resume path
+    resume_path = args.resume or os.getenv('RESUME_PATH')
+    if not resume_path:
+        raise ValueError("A resume file path must be provided either as an argument or through the RESUME_PATH environment variable.")
+
     # Read the resume
-    with open("/Users/oldbones/Network/syncthing/org/roam/job_hunt/resume.org") as file:
+    with open(resume_path) as file:
         resume = file.read()
 
     # Score the job match
